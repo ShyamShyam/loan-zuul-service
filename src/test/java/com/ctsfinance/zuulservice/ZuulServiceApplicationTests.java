4bf5a00ec03fba492c14456087cf5c8839b79831
unknown <ctsjavauser253@iiht.tech>
2020-08-25 00:19:06 +0530

    Adding Zuul service to git

---
+++ src/test/java/com/ctsfinance/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctsfinance.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
